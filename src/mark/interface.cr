ab997d9e5791d1bf50e55f625ded9a4e13551078
gernest <geofreyernest@live.com>
2016-04-11 20:30:06 +0300

    Namespace module to Mark

--- src/mark/interface.cr
+++ src/mark/interface.cr
@@ -1,4 +1,4 @@
-module Interface
+module Mark::Interface
   abstract class Parser
     abstract def parse
   end
--- src/mark/scanner.cr
+++ src/mark/scanner.cr
@@ -1,60 +1,62 @@
-enum Token
-  Asterisk            # *
-  CarriageReturn
-  Eof
-  Tab
-  Space
-  Newline
-  Hashtag
-  OpenBrace            #[
-  ClosingBrace         # ]
-  OpenBracket          #(
-  ClosingBracket       # )
-  Dash                 # -
-  Backtick             # `
-  Literal
-end
-
-class Scanner
-  @reader: Char::Reader 
-  def initialize(src : String)
-    @reader=Char::Reader.new(src)
-    @tokens=[] of Tuple
+module Mark
+  enum Token
+    Asterisk            # *
+    CarriageReturn
+    Eof
+    Tab
+    Space
+    Newline
+    Hashtag
+    OpenBrace            #[
+    ClosingBrace         # ]
+    OpenBracket          #(
+    ClosingBracket       # )
+    Dash                 # -
+    Backtick             # `
+    Literal
   end
 
-  def scan : Array(Tuple)
-    @reader.each do |ch|
-      case ch
-      when '*'
-        @tokens<<{Token::Asterisk,ch}
-      when '\r'
-        @tokens<<{Token::CarriageReturn,ch}
-      when '\0'
-        @tokens<<{Token::Eof,""}
-      when '\t'
-        @tokens<<{Token::Tab,ch}
-      when ' '
-        @tokens<<{Token::Space,ch}
-      when '\n'
-        @tokens<<{Token::Newline,ch}
-      when '#'
-        @tokens<<{Token::Hashtag,ch}
-      when '['
-        @tokens<<{Token::OpenBrace,ch}
-      when ']'
-        @tokens<<{Token::ClosingBrace,ch}
-      when '('
-        @tokens<<{Token::OpenBracket,ch}
-      when ')'
-        @tokens<<{Token::ClosingBracket,ch}
-      when '-'
-        @tokens<<{Token::Dash,ch}
-      when '`'
-        @tokens<<{Token::Backtick,ch}
-      else
-        @tokens<<{Token::Literal,ch}
-      end
+  class Scanner
+    @reader: Char::Reader 
+    def initialize(src : String)
+      @reader=Char::Reader.new(src)
+      @tokens=[] of Tuple
     end
-    @tokens
+
+    def scan : Array(Tuple)
+      @reader.each do |ch|
+        case ch
+        when '*'
+          @tokens<<{Token::Asterisk,ch}
+        when '\r'
+          @tokens<<{Token::CarriageReturn,ch}
+        when '\0'
+          @tokens<<{Token::Eof,""}
+        when '\t'
+          @tokens<<{Token::Tab,ch}
+        when ' '
+          @tokens<<{Token::Space,ch}
+        when '\n'
+          @tokens<<{Token::Newline,ch}
+        when '#'
+          @tokens<<{Token::Hashtag,ch}
+        when '['
+               @tokens<<{Token::OpenBrace,ch}
+        when ']'
+          @tokens<<{Token::ClosingBrace,ch}
+        when '('
+               @tokens<<{Token::OpenBracket,ch}
+        when ')'
+          @tokens<<{Token::ClosingBracket,ch}
+        when '-'
+          @tokens<<{Token::Dash,ch}
+        when '`'
+          @tokens<<{Token::Backtick,ch}
+        else
+          @tokens<<{Token::Literal,ch}
+        end
+      end
+      @tokens
+    end
   end
 end
